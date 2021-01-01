bcffb386e6f24f6f1151c2c6d2f813e98ac43a88
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-01 12:33:01 -0500

    add animation and sound

--- game.js
+++ game.js
@@ -5,5 +5,8 @@ const nextSequence = () => {
     let randomNumber = Math.floor(Math.random() * 4);
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
+    $('#' + randomChosenColor).fadeOut(100).fadeIn(100);
+    let audio = new Audio('sounds/' + randomChosenColor +'.mp3')
+    audio.play();
 };
 
