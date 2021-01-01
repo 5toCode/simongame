b5ecc9af0f3c0ae6cd9240e60d1b4d6ce4ee33fa
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-01 09:50:31 -0500

    complete step 2

--- game.js
+++ game.js
@@ -1,4 +1,9 @@
+let buttonColors = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
+
 const nextSequence = () => {
     let randomNumber = Math.floor(Math.random() * 4);
-    return randomNumber;
-}
+    let randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+};
+
