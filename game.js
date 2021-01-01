d8bf90bcfd6edba13ac80d9f2e75ab35402d1abe
mrobertsjr <70207997+mrobertsjr@users.noreply.github.com>
2021-01-01 13:53:23 -0500

    add animations for button presses

--- game.js
+++ game.js
@@ -7,12 +7,31 @@ const nextSequence = () => {
     let randomChosenColor = buttonColors[randomNumber];
     gamePattern.push(randomChosenColor);
     $('#' + randomChosenColor).fadeOut(100).fadeIn(100);
-    let audio = new Audio('sounds/' + randomChosenColor +'.mp3')
-    audio.play();
+    playSound(randomChosenColor);
 };
 
-$('div[type="button"]').click(function() {
+// Listen for a click on the buttons via class 'btn'
+$('.btn').click(function() {
+    // assign clicked button ID (color) and push into array
     let userChosenColor = this.id;
     userClickedPattern.push(userChosenColor);
-    console.log(userClickedPattern);
+    playSound(userChosenColor);
+    animatePress(userChosenColor);
+    // console.log(userClickedPattern);
 });
+
+// Function to play color-specific sound
+const playSound = name => {
+    let audio = new Audio('sounds/' + name +'.mp3')
+    audio.play();
+}
+
+// Animate when a button gets clicked
+const animatePress = currentColor => {
+    // create shortcut for button ID
+    let currentColorId = '#' + currentColor;
+    $(currentColorId).addClass('pressed');
+    setTimeout(function() {
+        $(currentColorId).removeClass('pressed');
+    }, 100);
+}
